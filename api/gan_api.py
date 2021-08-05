f274c55159f6620798fe52fd8f92e02988f415cf
akne <adenfun2@hotmail.com>
2021-08-06 09:24:08 +1000

    Request hashing

--- api/gan_api.py
+++ api/gan_api.py
@@ -1,12 +1,14 @@
 import random
+import os
 import numpy as np
 import tensorflow as tf
 
-from io import BytesIO
 from PIL import Image
 from tensorflow import keras
 from tensorflow.keras import layers
 from zipfile import ZipFile
+
+from tensorflow.python.keras.utils.generic_utils import default
 
 from cfg_parser import CFGParser
 
@@ -19,13 +21,44 @@ class GenerativeAPI():
         self.build_generator()
         self.model.load_weights("gen_weights.h5")
 
+    def hash_request(self, args):
+        """
+        Hashes the request to reduce computation for future
+        identical requests.
+        """
+        g = "-".join([str(arg) for arg in args])
+        return hash(g)
+
+    def check_exists(self, hash, ftype="image"):
+        """
+        Returns the file path if the request has already been fulfilled
+        previously.
+        """
+        path = CFGParser().get_path()
+        if ftype == "image":
+            path = "{0}/img/{1}.png".format(path, hash)
+        elif ftype == "zip":
+            path = "{0}/{1}.zip".format(path, hash)  
+        else:
+            path = "{0}/{1}.gif".format(path, hash)               
+
+        if os.path.exists(path):
+            return path, True
+        return path, False
+
+    def get_seed(self):
+        """
+        Returns a random integer between 0 and 10^8.
+        """
+        return random.randint(0, 1e8)
+
     def set_seed(self, seed=None):
         """
         Sets and returns the seed, generating one if one
         isn't supplied.
         """
         if seed is None:
-            seed = random.randint(0, 1e8)
+            seed = self.get_seed()
         random.seed(seed)
         tf.random.set_seed(seed)
 
@@ -43,17 +76,19 @@ class GenerativeAPI():
         Returns the path of the generated image and the seed.
         '''
         seed = self.set_seed(seed)
+        name = self.hash_request(["gen", seed])
+        path, exists = self.check_exists(name, "image")
+        if exists:
+            return path, seed
+
         noise = self.get_noise()
         pred = self.model.predict(noise)
         pred = ((0.5 * pred[0] + 0.5) * 256).astype("uint8")
 
-        buff = BytesIO()
         img = Image.fromarray(pred, "RGB")
-        img.save(buff, format="PNG")
-        fname = "{0}/img/gen-s{1}.png".format(CFGParser().get_path(), seed)
-        img.save(fname, format="PNG")
+        img.save(path, format="PNG")
 
-        return fname, seed
+        return path, seed
 
     def bulk(self, seed=None, amount=None):
         '''
@@ -63,28 +98,28 @@ class GenerativeAPI():
         seed = self.set_seed(seed)
         if amount is None or amount < 1:
             amount = CFGParser().get_amount()
+        name = self.hash_request(["bulk", amount, seed])
+        path, exists = self.check_exists(name, "zip")
+        if exists:
+            return path
 
         noise = self.get_noise(amount)
         pred = self.model.predict(noise)
         pred = ((0.5 * pred + 0.5) * 256).astype("uint8")
 
-        path = CFGParser().get_path()
         fnames = []
         for i, p in enumerate(pred):
-            buff = BytesIO()
             img = Image.fromarray(p, "RGB")
-            img.save(buff, format="PNG")
-            fname = "{0}/img/gen-b{1}-{2}.png".format(path, seed, i+1)
+            fname = "{0}/img/{1}-{2}.png".format(CFGParser().get_path(), name, i+1)
             fnames.append(fname)
             img.save(fname, format="PNG")
 
-        fzname = "{0}/bulk-s{1}-{2}.zip".format(path, seed, amount)
-        fzip = ZipFile(fzname, "w")
+        fzip = ZipFile(path, "w")
         for fname in fnames:
             fzip.write(fname)
         fzip.close()
 
-        return fzname
+        return path
 
     def interpolate(self, seeds=None, steps=None):
         '''
@@ -92,49 +127,47 @@ class GenerativeAPI():
         Returns the path of the zip containing the generated images.
         '''
         if seeds is None:
-            seeds = [random.randint(0, 1e8), random.randint(0, 1e8)]
+            seeds = [self.get_seed(), self.get_seed()]
         elif type(seeds) == str:
             seeds = [int(s) for s in seeds.split(",")]
         elif len(seeds) == 1:
-            seeds.append(random.randint(0, 1e8))
+            seeds.append(self.get_seed())
+
+        if steps is None:
+            steps = CFGParser().get_steps()
+
+        name = self.hash_request(["interpolate", steps, *seeds])
+        path, exists = self.check_exists(name, "zip")
+        if exists:
+            return path
 
         points = []
         for seed in seeds:
             self.set_seed(seed)
             points.append(self.get_noise())
         
-        if steps is None:
-            steps = CFGParser().get_steps()
-
         noise = []
-        for i in range(len(points)):
-            if i == 0:
-                continue
-            else:
-                percents = np.linspace(0, 1, num=steps)
-                i_noise = [((1.0 - p) * points[i-1]) + (p * points[i]) for p in percents]
-                noise.append(i_noise)
+        for i in range(1, len(points)):
+            percents = np.linspace(0, 1, num=steps)
+            i_noise = [((1.0 - p) * points[i-1]) + (p * points[i]) for p in percents]
+            noise.append(i_noise)
         noise = np.array(noise).reshape(((len(points) - 1) * steps, points[0].shape[1]))
         pred = self.model.predict(noise)
         pred = ((0.5 * pred + 0.5) * 256).astype("uint8")
 
-        path = CFGParser().get_path()
         fnames = []
         for i, p in enumerate(pred):
-            buff = BytesIO()
             img = Image.fromarray(p, "RGB")
-            img.save(buff, format="PNG")
-            fname = "{0}/img/gen-i{1}-{2}.png".format(path, "_".join([str(s) for s in seeds]), i+1)
+            fname = "{0}/img/{1}-{2}.png".format(CFGParser().get_path(), name, i+1)
             fnames.append(fname)
             img.save(fname, format="PNG")
         
-        fzname = "{0}/interpolate-s{1}-st{2}.zip".format(path, "_".join([str(s) for s in seeds]), steps)
-        fzip = ZipFile(fzname, "w")
+        fzip = ZipFile(path, "w")
         for fname in fnames:
             fzip.write(fname)
         fzip.close()
 
-        return fzname
+        return path
 
     def build_generator(self):
         '''
